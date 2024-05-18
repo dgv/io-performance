bc0a9d82bdb1a80f491c46beb4d44d2822c2b824
Daniel Gomes Vargas <dgv@fastmail.com>
2024-05-17 23:12:06 -0300

    processing revised

--- optimized.zig
+++ optimized.zig
@@ -30,30 +30,37 @@ pub fn main() anyerror!void {
             std.debug.print("{any}\n", .{err});
             break;
         };
+        const tRead2 = try std.time.Instant.now();
+        tRead += tRead2.since(tRead1);
         if (n == 0) {
             break;
         }
-        const tRead2 = try std.time.Instant.now();
-        tRead += tRead2.since(tRead1);
+
         for (buf) |c| {
+            // Found a whitespace char, count last word.
             if (c <= ' ') {
-                //const str = try std.ascii.allocLowerString(allocator, w.items);
-                const v = try words.getOrPut(w.items);
-                if (v.found_existing) {
-                    v.value_ptr.* += 1;
+                if (w.items.len > 0) {
+                    const v = try words.getOrPutValue(w.items, 1);
+                    if (v.found_existing) {
+                        v.value_ptr.* += 1;
+                    }
+                    _ = try w.toOwnedSlice(); // reset word buffer
                 }
-                _ = try w.toOwnedSlice();
                 continue;
             }
+
+            // Convert to ASCII lowercase as we go.
             var c_: u8 = undefined;
             if (c >= 'A' and c <= 'Z') {
-				c_ = c + ('a' - 'A');
-			}
+                c_ = c + ('a' - 'A');
+            }
+            // Add non-space char to word buffer.
             try w.append(c_);
         }
     }
-
     const tProcess = try std.time.Instant.now();
+
+    // not needed to convert into another slice for sorting
     const SortContext = struct {
         values: []u32,
         pub fn lessThan(ctx: @This(), a_index: usize, b_index: usize) bool {
@@ -65,7 +72,7 @@ pub fn main() anyerror!void {
 
     var out = std.io.getStdOut().writer();
     for (words.keys()) |k| {
-        try out.print("{s} {?}\n", .{ k, words.get(k) });
+        try out.print("{s} {d}\n", .{ k, words.get(k).? });
     }
     const tEnd = try std.time.Instant.now();
 
@@ -74,9 +81,9 @@ pub fn main() anyerror!void {
     const elSorting: f64 = @floatFromInt(tSort.since(tProcess));
     const elOutputting: f64 = @floatFromInt(tEnd.since(tSort));
     const elTotal: f64 = @floatFromInt(tEnd.since(tStart));
-    std.debug.print("Reading   : {d:.9}\n", .{elReading / std.time.ns_per_s});
-    std.debug.print("Processing: {d:.9}\n", .{elProcessing / std.time.ns_per_s});
-    std.debug.print("Sorting   : {d:.9}\n", .{elSorting / std.time.ns_per_s});
+    std.debug.print("Reading   : {d:.9}\n", .{elReading / std.time.ns_per_s}); // https://github.com/ziglang/zig/issues/17985
+    std.debug.print("Processing: {d:.9}\n", .{elProcessing / std.time.ns_per_s}); // assume capacity?
+    std.debug.print("Sorting   : {d:.9}\n", .{elSorting / std.time.ns_per_s}); // different from go the sorting order is constant
     std.debug.print("Outputting: {d:.9}\n", .{elOutputting / std.time.ns_per_s});
     std.debug.print("TOTAL     : {d:.9}\n", .{elTotal / std.time.ns_per_s});
 }
